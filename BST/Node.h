b921b1c54664f859d719396566221a66a5268d9f
yourespeakingtothemoon <manen@tutanota.com>
2023-05-22 11:30:32 -0600

    beestee

--- BST/Node.h
+++ BST/Node.h
@@ -1,15 +1,588 @@
-#pragma once
-namespace dsw {
+﻿#pragma once
+#include <string>
+#include <array>
 
+	template<typename T>
 	class Node
 	{
 	public:
-		Node(int value);
+		Node();
+		Node(T value);
 		~Node();
+		
+		void Add(T value);
+		Node<T>* Remove(T value, Node<T>* rt);
+		bool Contains(T value);
+		void Clear(Node<T>* rt);
 
-		int value;
+		Node<T>* GetLeft(Node<T>* n);
+		Node<T>* GetRight(Node<T>* n);
+		
+		int Count();
+		int Height();
+		
+		T* ToArray();
+		void ArrayBuilder(T* array, int index);
+		
+		bool DirectionChecker();
+		
+		void Visualizer(const std::string& prefix = "", bool isLeft = false);
+		
+		std::string InOrder();
+		std::string PreOrder();
+		std::string PostOrder();
+		
+		T value;
 		Node* left;
 		Node* right;
+		Node* parent;
 	};
-	};
-}
+
+	template<typename T>
+	inline Node<T>::Node()
+	{
+		//empty
+		this->value = NULL;
+		this->left = nullptr;
+		this->right = nullptr;
+		this->parent = nullptr;
+		
+	}
+
+	template<typename T>
+	inline Node<T>::Node(T value)
+	{
+		this->value = value;
+		this->parent = nullptr;
+		this->left = nullptr;
+		this->right = nullptr;
+	}
+
+	template<typename T>
+	inline Node<T>::~Node()
+	{
+		this->Clear();
+		//delete this;
+		
+	}
+
+	template<typename T>
+	inline void Node<T>::Add(T value)
+	{
+		if (this->value) {
+			if (value != this->value) {
+				if (value < this->value)
+				{
+					if (this->left == nullptr)
+					{
+						this->left = new Node(value);
+						this->left->parent = this;
+					}
+					else
+					{
+						this->left->Add(value);
+					}
+				}
+				else
+				{
+					if (this->right == nullptr)
+					{
+						this->right = new Node(value);
+						this->right->parent = this;
+					}
+					else
+					{
+						this->right->Add(value);
+					}
+				}
+			}
+			else {
+				return;
+			}
+		}
+		else
+		{
+			this->value = value;
+		}
+	}
+	
+	template<typename T>
+	inline Node<T>* Node<T>::GetLeft(Node<T>* n)
+	{
+		
+		if (n->left == nullptr)
+		{
+			return n;
+				//recursive black magic again
+		}
+		else
+		{
+			return GetLeft(n->left);
+		}
+	
+	}
+	
+	template<typename T>
+	inline Node<T>* Node<T>::GetRight(Node<T>* n)
+	{
+		if (n->right == nullptr)
+		{
+			return n;
+			//recursive black magic a third time
+		}
+		else
+		{
+			return GetLeft(n->right);
+		}
+	}
+
+	template<typename T>
+	inline Node<T>* Node<T>::Remove(T value, Node<T>* rt)
+	{
+		//we know contains works, lol
+		if (this->Contains(value))
+		{
+			//attempt 2
+			if (rt == nullptr)
+			{
+				return this;
+			}
+			else {
+
+				if (value < rt->value)
+				{
+					rt->left = Remove(value, rt->left);
+				}
+				else if (value > rt->value)
+				{
+					rt->right = Remove(value, rt->right);
+				}
+				else
+				{
+					if (rt->left == nullptr)
+					{
+						return rt->right;
+
+					}
+					else if (rt->right == nullptr)
+					{
+						return rt->left;
+					}
+
+
+					rt->value = GetLeft(rt->right)->value;
+					rt->right = Remove(rt->value, rt->right);
+
+
+				}
+				return rt;
+			}
+		}
+		else { return rt; }
+		}
+		
+		//Node* nodeToRemove = nullptr;
+		//int direction = 0;
+
+		////find node
+		//if (value < this->value)
+		//{
+		//	if (this->left == nullptr)
+		//	{
+		//		return;
+		//	}
+		//	else if (this->left->value == value)
+		//	{
+		//		nodeToRemove = this->left;
+		//		direction = -1;
+		//	}
+		//	else
+		//	{
+		//		this->left->Remove(value);
+		//	}
+		//}
+		//else if (value > this->value)
+		//{
+		//	if (this->right == nullptr)
+		//	{
+		//		return;
+		//	}
+		//	else if (this->right->value == value)
+		//	{
+		//		nodeToRemove = this->right;
+		//		direction = 1;
+		//	}
+		//	else
+		//	{
+		//		this->right->Remove(value);
+		//	}
+
+		//}
+		//else
+		//{
+		//	nodeToRemove = this;
+		//}
+		//
+		//if (nodeToRemove) {
+		//	//if node is a leaf, delete it
+		//	if (!nodeToRemove->left && !nodeToRemove->right) {
+		//		delete nodeToRemove;
+		//		return;
+		//	}
+
+		//	//if node has one child, replace it with its child
+		//	if (nodeToRemove->left || nodeToRemove->right)
+		//	{
+		//		switch (direction)
+		//		{
+		//		case -1:
+		//			this->left = nodeToRemove->left;
+		//			break;
+		//		case 1:
+		//			this->right = nodeToRemove->left;
+		//			break;
+
+		//		}
+		//		// if node to remove also has a right child make that child the child of the left node
+		//		
+		//	}
+		//	if (nodeToRemove->right && nodeToRemove->left)
+		//	{
+		//		switch (direction)
+		//		{
+		//		case -1:
+		//			this->left->right = nodeToRemove->right;
+		//			break;
+		//		case 1:
+		//			this->right->right = nodeToRemove->right;
+		//			break;
+
+		//		}
+
+
+		//	}
+		//}
+
+	
+
+	template<typename T>
+	inline bool Node<T>::Contains(T value)
+	{
+		//traverse
+		if (this->value == value)
+		{
+			return true;
+		}
+		else
+		{
+			if (value < this->value)
+			{
+				if (this->left == nullptr)
+				{
+					return false;
+				}
+				else
+				{
+					return this->left->Contains(value);
+				}
+			}
+			else
+			{
+				if (this->right == nullptr)
+				{
+					return false;
+				}
+				else
+				{
+					return this->right->Contains(value);
+				}
+			}
+		}
+	}
+
+	template<typename T>
+	inline void Node<T>::Clear(Node<T>* n)
+	{
+		if (n==nullptr) {
+			return;
+		}
+		// recursive black magic ig lol
+		if (n.left != nullptr)
+		{
+			Clear(n.left);
+		}
+		if (n.right != nullptr)
+		{
+			Clear(n.right);
+		}
+		
+		delete n;
+		n = nullptr;
+	}
+	
+
+	template<typename T>
+	inline int Node<T>::Count()
+	{
+		if (this)
+		{
+			int count= 1;
+			return count + this->left->Count() + this->right->Count();
+		}
+		else
+		{
+			//if null return 0
+			return 0;
+		}
+	}
+
+	template<typename T>
+	inline int Node<T>::Height()
+	{
+		if (this)
+		{
+			int leftHeight = 0;
+			int rightHeight = 0;
+			if (this->left)
+			{
+				leftHeight = this->left->Height();
+			}
+			if (this->right)
+			{
+				rightHeight = this->right->Height();
+			}
+			if (leftHeight > rightHeight)
+			{
+				return leftHeight + 1;
+			}
+			else
+			{
+				return rightHeight + 1;
+			}
+		}
+		else
+		{
+			//if null return 0
+			return 0;
+		}
+	}
+
+	template<typename T>
+	inline T* Node<T>::ToArray()
+	{
+		if (this->value) {
+			//create array
+			T* array = new T[this->Count()];
+			this->ArrayBuilder(array, 0);
+			//return array
+			return array;
+		}
+		else {
+			return nullptr;
+		}
+		
+	}
+
+
+
+	template<typename T>
+	inline void Node<T>::ArrayBuilder(T* array, int index)
+	{
+		
+		//if left exists, add it to the array
+		if (this->left)
+		{
+			this->left->ArrayBuilder(array, index);
+		}
+		if (!this->left && !this->right)
+		{
+			array[index] = this->value;
+			index++;
+		} 
+		//if right exists, add it to the array
+		if (this->right)
+		{
+			this->right->ArrayBuilder(array, index + 1);
+		}
+	}
+
+	template<typename T>
+	inline bool Node<T>::DirectionChecker()
+	{
+		if (this->parent ==nullptr)
+		{
+			return true;
+		}
+		else {
+			if (this->value > this->parent->value)
+			{
+				
+			return this->parent->DirectionChecker();
+			}
+			else
+			{
+				return false;
+			}
+		}
+		
+	}
+
+	template<typename T>
+	inline void Node<T>::Visualizer(const std::string& prefix, bool isLeft)
+	{
+		//built using GPT4
+		/*if (right) {
+			right->Visualizer(prefix + (isLeft ? "│   " : "    "), false);
+		}
+
+		std::cout << prefix;
+		std::cout << (isLeft ? "└── " : "┌── ");
+		std::cout << value << std::endl;
+
+		if (left) {
+			left->Visualizer(prefix + (isLeft ? "    " : "│   "), true);
+		}*/
+		//adjusted because codepage issues
+	/*	if (right) {
+			right->Visualizer(prefix + (isLeft ? "|   " : "    "), false);
+		}
+
+		std::cout << prefix;
+		std::cout << (isLeft ? "\\-- " : "/-- ");
+		std::cout << value << std::endl;
+
+		if (left) {
+			left->Visualizer(prefix + (isLeft ? "    " : "|   "), true);
+		}*/
+	/*	if (right) {
+			right->Visualizer(prefix + (isLeft ? "     " : " |   "), false);
+		}
+
+		std::cout << prefix;
+
+		if (isLeft) {
+			std::cout << " \\-- ";
+		}
+		else {
+			std::cout << " /-- ";
+		}
+
+		std::cout << value << std::endl;
+
+		if (left) {
+			left->Visualizer(prefix + (isLeft ? " |   " : "     "), true);
+		}*/
+		if (right) {
+			right->Visualizer(prefix + (isLeft ? "     " : " |   "), false);
+		}
+
+		std::cout << prefix;
+
+		if (isLeft) {
+			std::cout << " /-- ";
+		}
+		else {
+			std::cout << " \\-- ";
+		}
+
+		std::cout << value << std::endl;
+
+		if (left) {
+			left->Visualizer(prefix + (isLeft ? " |   " : "     "), true);
+		}
+		//Im just gonna deal with it looking awkward
+	
+	}
+
+	template<typename T>
+	inline std::string Node<T>::InOrder()
+	{
+		std::string out = "";
+		//int count = 0;
+		//count++;
+		if (this) {
+			//count -> this->Count();
+			if (this->left)
+			{
+				out += this->left->InOrder();
+			}
+			
+				if (this->DirectionChecker()) {
+			if (!this->right && !this->left)
+			{
+				//its a leaf
+					out += std::to_string(this->value);
+				}
+			if (!this->right)
+			{
+				out += std::to_string(this->value);
+			}
+			else
+			{
+					out += std::to_string(this->value)+", ";
+				}
+				
+			}
+			else
+			{
+				//not a leaf
+					out += std::to_string(this->value)+", ";
+				
+			}
+			
+			
+
+			if (this->right)
+			{
+
+				out += this->right->InOrder();
+
+			}
+		}
+		return out;
+	}
+
+	template<typename T>
+	inline std::string Node<T>::PreOrder()
+	{
+		std::string out = "";
+		if (this) {
+
+			out += std::to_string(this->value);
+
+
+			//	out += std::to_string(this->value) + ", ";
+			if (this->left) {
+				out += ", " + this->left->PreOrder();
+			}
+			if (this->right) {
+				out += ", " + this->right->PreOrder();
+			}
+			return out;
+		}
+	}
+
+	template<typename T>
+	inline std::string Node<T>::PostOrder()
+	{
+		std::string out = "";
+		if (this) {
+			if (this->left)
+			{
+				out += this->left->PostOrder();
+			}
+			if (this->right)
+			{
+				out += this->right->PostOrder();
+			}
+			if (this->parent == nullptr)
+			{
+				out += std::to_string(this->value);
+			}
+			else {
+				out += std::to_string(this->value) + ", ";
+			}
+		}
+		return out;
+	}
+	
--- BST/main.cpp
+++ BST/main.cpp
@@ -2,11 +2,347 @@
 //
 
 #include <iostream>
+#include <algorithm>
+#include <windows.h>
+
+#include "Node.h"
+
+
+class BSTTests
+{
+public:
+	BSTTests()
+	{
+		//this->huh = 1;
+	}
+	bool AddTest1()
+	{
+		Node<int>* n = new Node<int>(40);
+		n->Add(50);
+		n->Add(45);
+		n->Add(35);
+		n->Add(30);
+		//std::cout << n->PostOrder();
+		if (n->PostOrder() == "30 35 45 50 40")
+		{
+			std::cout << "AddTest1 Works (if PostOrder Tests dont work this may be unreliable)";
+			return true;
+		}
+		return false;
+		//n->Visualizer("", false);
+	}
+	//another add test
+	
+	bool AddTestBasic()
+	{
+		Node<int>* n = new Node<int>(40);
+		n->Add(50);
+		n->Add(45);
+		n->Add(35);
+		n->Add(30);
+		//std::cout << n->PostOrder();
+		if (n->PostOrder() == "30, 35, 45, 50, 40")
+		{
+			std::cout << "AddTest1 Works (if PostOrder Tests dont work this may be unreliable)" << std::endl;
+			return true;
+		}
+		std::cout << n->PostOrder();
+		//n->Visualizer("",false);
+		return false;
+		//n->Visualizer("", false);
+	}
+	//if add test basic works do other tests as Basic is the foundation that the thing even works
+
+	//Actual Add tests
+	bool AddTestEmpty()
+	{
+		Node<int>* n = new Node<int>();
+		n->Add(100);
+		if (n->value == 100)
+		{
+			std::cout << "AddTest2 Works" << std::endl;
+			return true;
+		}
+		return false;
+	}
+
+	bool AddTestDupe()
+	{
+		//should just return without adding nothing
+		Node<int>* n = new Node<int>(5);
+		n->Add(2);
+		n->Add(3);
+		n->Add(3);
+		if (n->InOrder() == "2, 3, 5")
+		{
+			std::cout << "AddTest3 Works" << std::endl;
+			return true;
+		}
+		else { 
+			std::cout << n->InOrder();
+			return false; }
+	}
+
+	//Contains tests
+
+	bool ConatinTestEmpty()
+	{
+		Node<int>* n = new Node<int>();
+
+		std::cout << "ContainTestEmpty" << std::endl;
+		n->Visualizer();
+		return !n->Contains(5);
+	}
+	bool ContainTestLarge()
+	{
+		Node<int>* n = new Node<int>(500);
+		int y;
+		for (int x = 0; x < 150; x++)
+		{
+			int num = rand();
+			n->Add(num);
+			if (x == 145) {
+				y = num;
+			}
+		}
+		std::cout << "ContainTestLarge" << std::endl;
+		//n->Visualizer();
+		//std::cout << y << std::endl;
+		return n->Contains(y);
+
+
+	}
+
+	//Removes Tests
+	bool RemoveTestNoExist()
+	{
+		Node<int>* n = new Node<int>(50);
+		n->Add(150);
+		n->Add(25);
+		n->Add(12);
+		n->Add(200);
+		std::string before = "12, 25, 200, 150, 50";
+		n->Remove(55,n);
+	//	n->Remove(45,n);
+
+		std::cout << "RemoveTestNoExist" << std::endl;
+		std::cout << before << std::endl;
+		std::cout << n->PostOrder() << std::endl;
+		//n->Visualizer();
+		return true;
+
+	}
+
+	bool RemoveTestRootRemove()
+	{
+		Node<int>* test = new Node<int>(30);
+		test->Add(25);
+		test->Add(15);
+
+		Node<int>* n = new Node<int>(26);
+		n->Add(25);
+		n->Add(15);
+		n->Add(30);
+
+		n->Remove(26,n);
+
+		std::cout << "RemoveTestRootRemove" << std::endl;
+		std::cout << n->PostOrder() << std::endl;
+		std::cout << test->PostOrder() << std::endl;
+		n->Visualizer();
+		return n->PostOrder() == test->PostOrder();
+	}
+
+	//Clear tests?
+
+	bool ClearTest()
+	{
+		Node<int>* n = new Node<int>(26);
+		n->Add(25);
+		n->Add(15);
+		n->Add(30);
+
+		n->Clear(n);
+
+		std::cout << "Cleartest" << std::endl;
+		std::cout << n->PostOrder() << std::endl;
+		return n == NULL;
+
+	}
+
+	//Count Test
+
+	bool CountTestEmpty()
+	{
+		Node<int>* n = new Node<int>();
+		std::cout << "CountTestEmpty" << std::endl;
+		return n->Count() == 1;
+	}
+	bool CountTest()
+	{
+		Node<int>* n = new Node<int>(50);
+		std::cout << "CountTest" << std::endl;
+		n->Add(25);
+		n->Add(75);
+
+		return n->Count() == 3;
+	}
+
+	//Height test
+
+	bool HeightTestBalanced()
+	{
+		Node<int>* n = new Node<int>(50);
+		n->Add(25);
+		n->Add(75);
+		std::cout << "HeightTest" << std::endl;
+		return n->Height() == 2;
+	}
+	bool HeightTestUnbalanced()
+	{
+		Node<int>* n = new Node<int>(50);
+		n->Add(25);
+		n->Add(75);
+		n->Add(60);
+		n->Add(65);
+		std::cout << "HeightTestUnbalanced" << std::endl;
+		return n->Height() == 4;
+	}
+
+	//ToArray Tests
+
+	bool ToArrayTest()
+	{
+		Node<int>* n = new Node<int>(9);
+		n->Add(8);
+		n->Add(12);
+		n->Add(11);
+		n->Add(7);
+		n->Add(5);
+		n->Add(1);
+		int test[] = {
+			1,5,7,8,9,11,12
+		};
+		std::cout << "ToArraytest" << std::endl;
+		std::cout << std::to_string(n->ToArray()[0]) + std::to_string(n->ToArray()[1]) + std::to_string(n->ToArray()[2]) + std::to_string(n->ToArray()[3]) + std::to_string(n->ToArray()[4]) + std::to_string(n->ToArray()[5]) + std::to_string(n->ToArray()[6]);
+		return n->ToArray() == test;
+	}
+	bool ToArrayTestEmpty()
+	{
+		Node<int>* n = new Node<int>();
+		int test[1] = {};
+		std::cout << "ToArrayEmpty" << std::endl;
+		//s//td::cout << n->ToArray();
+		return !n->ToArray();
+	}
+
+	//Basic Order Tests
+	bool InTestBasic()
+	{
+		Node<int>* n = new Node<int>(9);
+		n->Add(8);
+		n->Add(12);
+		n->Add(11);
+		std::cout << "InTest" << std::endl;
+		std::cout << n->InOrder();
+		return n->InOrder() == "8, 9, 11, 12";
+	}
+	bool InTestEmpty()
+	{
+		Node<int>* n = new Node<int>();
+		std::cout << "InTestEmpty" << std::endl;
+	//	std::cout << n->InOrder();
+		return n->InOrder() == "00";
+	}
+
+	bool PreTestBasic()
+	{
+		Node<int>* n = new Node<int>(9);
+		n->Add(8);
+		n->Add(12);
+		n->Add(11);
+		std::cout << "PreTestBasic" << std::endl;
+		std::cout << n->PreOrder();
+		return n->PreOrder() == "9, 8, 12, 11";
+	}
+	bool PreTestPostTestOneNode()
+	{
+		Node<int>* n = new Node<int>(9);
+		std::cout << "PreTestPostTestOne" << std::endl;
+		return n->PostOrder() == n->PreOrder();
+	}
+	bool PreTestPostTestNotMatch()
+	{
+		Node<int>* n = new Node<int>(9);
+		n->Add(8);
+		n->Add(12);
+		n->Add(11);
+		std::cout << "PreTestPostTest" << std::endl;
+		return n->PostOrder() != n->PreOrder();
+	}
+	bool PostTestBasic()
+	{
+		Node<int>* n = new Node<int>(9);
+		n->Add(8);
+		n->Add(12);
+		n->Add(11);
+		std::cout << "PostTest" << std::endl;
+		return n->PostOrder() == "8, 11, 12, 9";
+	}
+
+
+private:
+	int huh = 0;
+};
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	/*Node<int>* n = new Node<int>(40);
+	n->Add(50);
+	n->Add(45);
+	n->Add(35);
+	n->Add(30);
+	std::cout << n->PostOrder();*/
+	BSTTests* b = new BSTTests();
+	
+	if (b->AddTestBasic())
+	{
+		std::cout << b->AddTestEmpty() << std::endl;
+		std::cout << b->AddTestDupe() << std::endl;
+		std::cout << b->ConatinTestEmpty() << std::endl;
+		std::cout << b->ContainTestLarge() << std::endl;
+		std::cout << b->RemoveTestRootRemove() << std::endl;
+		std::cout << b->RemoveTestNoExist() << std::endl;
+		std::cout << b->ClearTest() << std::endl;
+		std::cout << b->CountTest() << std::endl;
+		std::cout << b->CountTestEmpty() << std::endl;
+		std::cout << b->HeightTestBalanced() << std::endl;
+		std::cout << b->HeightTestUnbalanced() << std::endl;
+		std::cout << b->ToArrayTest() << std::endl;
+		std::cout << b->ToArrayTestEmpty() << std::endl;
+		std::cout << b->InTestBasic() << std::endl;
+		std::cout << b->InTestEmpty() << std::endl;
+		std::cout << b->PreTestBasic() << std::endl;
+		std::cout << b->PreTestPostTestNotMatch() << std::endl;
+		std::cout << b->PreTestPostTestOneNode() << std::endl;
+		std::cout << b->PostTestBasic() << std::endl;
+		std::cout << "if all 1s i've won";
+
+	}
+	else {
+		std::cout << "I lose outright ;(";
+	}
+
+	//std::cout << 
+
 }
+
+//test methods for Node Class
+//2 for each method
+
+
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
@@ -17,4 +353,4 @@ int main()
 //   3. Use the Output window to see build output and other messages
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln f
